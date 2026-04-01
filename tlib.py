41a5d75f0a5d1e73463851df5d5a0110a7d4b208
Sajak <sajakbasnet77@gmail.com>
2026-04-01 13:29:43 +0545

    minor fix

--- tlib.py
+++ tlib.py
@@ -2,5 +2,5 @@ import pimakerlibrary as pimaker
 
 
 if __name__ == "__main__":
-	pimaker.detect_fingertip() # Detect only ring finger
+	pimaker.detect_fingertip(1) # Detect only ring finger
     # pimaker.open_camera()

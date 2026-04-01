3d4d25dc13cdcefc1a6f0355095d55c01664d2a3
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2026-04-01 13:22:12 +0545

    feat: eye detection

--- pimakerlibrary/__init__.py
+++ pimakerlibrary/__init__.py
@@ -1,4 +1,5 @@
 from .core import make_pi
 from .camera import open_camera, detect_fingertip
+from .eye_detect import detect_eyes
 
-__all__ = ["make_pi", "open_camera", "detect_fingertip"]
+__all__ = ["make_pi", "open_camera", "detect_fingertip", "detect_eyes"]
